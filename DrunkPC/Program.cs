30a325cb8f86e8fbfcec666956ee3ef7872473df
SilasA <agnewsilas@gmail.com>
2016-03-10 14:13:26 -0500

    More stuffs

--- DrunkPC/Program.cs
+++ DrunkPC/Program.cs
@@ -27,26 +27,57 @@ namespace DrunkPC
         {
             Console.WriteLine("DrunkPC prank App.");
 
+            Thread drunkMouseThread = new Thread(new ThreadStart(DrunkMouseThread));
+            Thread drunkKeyboardThread = new Thread(new ThreadStart(DrunkKeyboardThread));
+            Thread drunkSoundThread = new Thread(new ThreadStart(DrunkSoundThread));
+            Thread drunkPopUpthread = new Thread(new ThreadStart(DrunkPopUpthread));
+
+            Console.Read();
+
+            drunkMouseThread.Start();
+            drunkKeyboardThread.Start();
+            drunkSoundThread.Start();
+            drunkPopUpthread.Start();
         }
 
         static void DrunkMouseThread()
         {
+            Console.WriteLine("DrunkMouseThread Started");
 
+            while (true)
+            {
+                Thread.Sleep(500);
+            }
         }
 
         static void DrunkKeyboardThread()
         {
+            Console.WriteLine("DrunkKeyboardThread Started");
 
+            while (true)
+            {
+                Thread.Sleep(500);
+            }
         }
 
         static void DrunkSoundThread()
         {
+            Console.WriteLine("DrunkSoundThread Started");
 
+            while (true)
+            {
+                Thread.Sleep(500);
+            }
         }
 
         static void DrunkPopUpthread()
         {
+            Console.WriteLine("DrunkPopUpthread Started");
 
+            while (true)
+            {
+                Thread.Sleep(500);
+            }
         }
     }
 }
