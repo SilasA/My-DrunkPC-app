1d8fe8bdd4ac2f1939f5ec039c7d122a247144b9
SilasA <agnewsilas@gmail.com>
2016-03-10 14:01:58 -0500

    Initial Commit of my versions of Barnacules's app

---
+++ DrunkPC/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+//
+// Aplication Name: DrunkPC (My version of Barnacules Nerdgasm's tutorial)
+// Description: Application that generates erratic mouse and keyboard movements
+// and input and generates system sounds and fake dialog to confuse the user.
+//
+
+
+namespace DrunkPC
+{
+    class Program
+    {
+        public static Random _random = new Random();
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DrunkPC prank App.");
+
+        }
+
+        static void DrunkMouseThread()
+        {
+
+        }
+
+        static void DrunkKeyboardThread()
+        {
+
+        }
+
+        static void DrunkSoundThread()
+        {
+
+        }
+
+        static void DrunkPopUpthread()
+        {
+
+        }
+    }
+}
